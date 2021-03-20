148373ace2cf75eafad5ee9ff852550585c3aa5d
PyRPy <44626163+PyRPy@users.noreply.github.com>
2021-03-20 11:53:34 -0400

    random numbers, and histograms

---
+++ 001_random_numbers.R
@@ -0,0 +1,7 @@
+# how to produce random numbers and plot its profile
+
+random_numbers <- rnorm(100, 0, 1)
+plot(random_numbers)
+abline(h=0)
+
+# this produces 100 random numbers with normal distribution
---
+++ 002_histogram_basic.R
@@ -0,0 +1,13 @@
+# how to produce 100 random numbers and produce a histogram
+
+# produce 100 random numbers
+random_numbers <- rnorm(100, 0, 1)
+hist(random_numbers)
+
+# look into the function for help
+
+hist(random_numbers, breaks = 10)
+
+# not helpful
+
+# thank you !
