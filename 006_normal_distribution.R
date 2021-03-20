3b3928d732be78959e75986af34231f3869b3cf1
PyRPy <44626163+PyRPy@users.noreply.github.com>
2021-03-20 14:59:49 -0400

    normal distribution

---
+++ 006_normal_distribution.R
@@ -0,0 +1,14 @@
+# normal distribution
+
+# get some numbers
+myNormalNumbers <- rnorm(10000, 0, 1)
+
+# histogram
+hist(myNormalNumbers)
+
+# add more numbers
+# like a 'bell' ?
+# add more numbers
+# good enough ?
+
+# Thanks for watching !
