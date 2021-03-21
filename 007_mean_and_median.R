75000544a5927cb9fc26f7e309c7fcc1436bb998
PyRPy <44626163+PyRPy@users.noreply.github.com>
2021-03-21 10:32:23 -0400

    mean, median, var, and sd in R

---
+++ 007_mean_and_median.R
@@ -0,0 +1,24 @@
+
+# Mean and median ---------------------------------------------------------
+
+# let's generate some numbers first
+myNumbers <- c(1, 2, 3, 2, 5, 6, 9 , 18, 11, 3, 4, 6, 7)
+
+# length of numbers
+length(myNumbers)
+
+# find mean
+mean(myNumbers)
+
+# pay attention to upper or lower case in R
+mean(mynumbers)
+
+# will be wrong
+
+# find median
+median(myNumbers)
+
+# if we change one of numbers
+# mean changes, but median stays same or called 'resistance'
+
+# THANK YOU FOR WATCHING !
---
+++ 008_variance_standard_deviation.R
@@ -0,0 +1,26 @@
+
+# Variance and standard deviation -----------------------------------------
+
+# Today's topic
+
+# let's get some numbers
+myNumbers <- c(2, 4, 3, 8, 1, 11, 14, 0, 3, 12)
+
+# we have 10 numbers, check it in environment on the right
+
+# find the variance
+var(myNumbers)
+
+# find the sd
+sd(myNumbers)
+
+# how to find the relationship between variance and sd ?
+# check this out :
+
+sqrt(var(myNumbers))
+
+sqrt(var(myNumbers)) == sd(myNumbers)
+
+# we got 'true' , so it is equivalent between 'sqrt(var(myNumbers)) == sd(myNumbers)'
+
+# THANK YOU FOR WATCHING !
