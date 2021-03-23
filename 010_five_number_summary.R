17bb1cb14f8cfa25ce2236c9066873e5cf2e39c0
PyRPy <44626163+PyRPy@users.noreply.github.com>
2021-03-23 10:15:39 -0400

    five number summary and boxplot

---
+++ 010_five_number_summary.R
@@ -0,0 +1,13 @@
+
+# Five number summary -----------------------------------------------------
+
+# let's create some numbers 
+
+myNumbers <- c(3, 2, 4, 6, 1, 6, 9, 11, 3, 4)
+
+# just simply use 'summary()' function in R
+summary(myNumbers)
+
+# then you can 5 number summary
+
+# min, Q1, Q3, median, max
---
+++ 011_boxplot.R
@@ -0,0 +1,20 @@
+
+# boxplot -----------------------------------------------------------------
+
+# get some numbers first
+myNumbers <- c(3, 2, 4, 6, 1, 6, 9, 11, 3, 4)
+
+# boxplot in R
+boxplot(myNumbers, horizontal = TRUE)
+
+# compare to 5 numbers in summary
+summary(myNumbers)
+
+# median is 4, bold line in the middle of 'box'
+
+# how to make it horizontal ?
+
+# we may have an outlier !
+# which is 11 !
+
+# THANKS FOR WATCHING !
