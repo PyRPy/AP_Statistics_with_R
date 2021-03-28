95c55364e7b3eae9cef5fea2c58f99eb4ccc351b
PyRPy <44626163+PyRPy@users.noreply.github.com>
2021-03-28 10:40:24 -0400

    Unit 3: Collecting Data - sample, experiment
    random permutations
    starting of the experiment design

---
+++ 023_random_permutations.R
@@ -0,0 +1,12 @@
+# how to make random permutation
+
+dice <- 1:6
+dice
+# let's shuffle the numbers
+sample(dice)
+
+sample(dice)
+
+# every time you got something different
+
+# THANKS FOR WATCHING !
