6fa2dae2efc7e484f6f81b831797b2f79d3c2b13
PyRPy <44626163+PyRPy@users.noreply.github.com>
2021-11-18 10:12:42 -0500

    java review for AP CS

---
+++ AP_java/MyExamples.java
@@ -0,0 +1,11 @@
+// AP CS preparation programs in Java
+public class MyExamples {
+
+	// Main driver method
+	public static void main(String[] args)
+	{
+
+		// Print statement
+		System.out.println("test of javac");
+	}
+}
